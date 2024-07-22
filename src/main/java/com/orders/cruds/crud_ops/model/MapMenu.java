9432df382d939a892ea521fb6b2cb032dd0aae60
Sapnadip Baidya <sapnadipbaidya@Sapnadips-MacBook-Pro.local>
2024-07-23 03:21:43 +0530

    field support

--- src/main/java/com/orders/cruds/crud_ops/model/MapMenu.java
+++ src/main/java/com/orders/cruds/crud_ops/model/MapMenu.java
@@ -9,7 +9,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "map_menu")
 public class MapMenu {
@@ -19,52 +18,72 @@ public class MapMenu {
 	private Integer mapLayoutId;
 	@Column(name = "priority_id")
 	private Integer layoutName;
-	@Column(name =  "component_path")
+	@Column(name = "component_path")
 	private String componentPath;
-	@Column(name =  "component_route")
+	@Column(name = "component_route")
 	private String componentRoute;
+	@Column(name = "component_name")
+	private String componentName;
 	@ManyToOne
 	@JoinColumn(name = "fk_menu_id")
 	private LuMenu fkMenuId;
 	@ManyToOne
 	@JoinColumn(name = "fk_datatype_id")
 	private DataType fkDataTypeId;
-	
-	
+
 	public Integer getMapLayoutId() {
 		return mapLayoutId;
 	}
+
 	public void setMapLayoutId(Integer mapLayoutId) {
 		this.mapLayoutId = mapLayoutId;
 	}
+
 	public Integer getLayoutName() {
 		return layoutName;
 	}
+
 	public void setLayoutName(Integer layoutName) {
 		this.layoutName = layoutName;
 	}
+
 	public LuMenu getFkMenuId() {
 		return fkMenuId;
 	}
+
 	public void setFkMenuId(LuMenu fkMenuId) {
 		this.fkMenuId = fkMenuId;
 	}
+
 	public DataType getFkDataTypeId() {
 		return fkDataTypeId;
 	}
+
 	public void setFkDataTypeId(DataType fkDataTypeId) {
 		this.fkDataTypeId = fkDataTypeId;
 	}
+
 	public String getComponentPath() {
 		return componentPath;
 	}
+
 	public void setComponentPath(String componentPath) {
 		this.componentPath = componentPath;
 	}
+
 	public String getComponentRoute() {
 		return componentRoute;
 	}
+
 	public void setComponentRoute(String componentRoute) {
 		this.componentRoute = componentRoute;
+	}
+
+	public String getComponentName() {
+		return componentName;
+	}
+
+	public void setComponentName(String componentName) {
+		this.componentName = componentName;
 	}
 }
